5041a37e254a50fac5c1ad28a5dd6ddde7338f6f
Nellybii <nellychepkem2015@gmail.com>
2024-12-13 14:26:56 +0300

    middleware folder

--- Routes/BookRoutes.js
+++ Routes/BookRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const Router = express.Router();
-const authenticate = require("../authmiddleware");
+const authenticate = require("../middwares/authmiddleware");
 
 const bookController = require('../Controllers/BookController');
 
 Router.post('/books', bookController.createBook);
 Router.post('/books/:id/borrow', authenticate, bookController.borrowBook);
 Router.post('/books/:id/return', authenticate, bookController.returnBook);
-Router.get('/books', authenticate, bookController.getAllBooks);
+Router.get('/books', bookController.getAllBooks);
 
 module.exports = Router;
