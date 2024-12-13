1c1928423cb7ab4d84e0132f8d19cac09374497c
Nellybii <nellychepkem2015@gmail.com>
2024-12-13 22:32:14 +0300

    vercel

--- Routes/BookRoutes.js
+++ Routes/BookRoutes.js
@@ -6,6 +6,6 @@ const bookController = require('../Controllers/BookController');
 router.post('/books', authenticate.authenticate, authenticate.isAdmin, bookController.createBook);
 router.put('/books/:id/borrow', authenticate.authenticate, bookController.borrowBook);
 router.put('/books/:id/return', authenticate.authenticate, bookController.returnBook);
-router.get('/books', authenticate.authenticate, bookController.getAllBooks);
+router.get('/books', bookController.getAllBooks);
 
 module.exports = router;
